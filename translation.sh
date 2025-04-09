48ab1ced68b79ea8992dc39a9abc1922a6d45dd2
capsmalt <capsmalt@gmail.com>
2025-04-09 13:08:29 +0900

    Update translation.sh
    Change repo url

--- translation.sh
+++ translation.sh
@@ -5,6 +5,6 @@ for ((i=1; i<=line_count; i++)); do
     NS="showroom-user"$i
     oc scale -n $NS deploy/showroom --replicas=0
     sleep 1
-    oc patch -n $NS deploy/showroom --patch='{"spec":{"template":{"spec":{"containers": [{"name": "content","env": [{"name": "GIT_REPO_URL", "value": "https://github.com/RH-OPEN/openshift-virt-roadshow-cnv-multi-user-jp-2025"}, {"name": "GIT_REPO_REF", "value": "main"}]}]}}}}'
+    oc patch -n $NS deploy/showroom --patch='{"spec":{"template":{"spec":{"containers": [{"name": "content","env": [{"name": "GIT_REPO_URL", "value": "https://github.com/loungeplus/virt-v2"}, {"name": "GIT_REPO_REF", "value": "main"}]}]}}}}'
     oc scale -n $NS deploy/showroom --replicas=1
 done
